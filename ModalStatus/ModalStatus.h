28dd74bb4264f0f1fdf5682a1ff229cc074c6ce2
lochiwei <lochiwei.ios.dev.free@gmail.com>
2018-07-19 09:02:10 +0800

    Initial Commit

---
+++ ModalStatus/ModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  ModalStatus.h
+//  ModalStatus
+//
+//  Created by pegasus on 2018/07/19.
+//  Copyright © 2018年 Lo Chiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModalStatus.
+FOUNDATION_EXPORT double ModalStatusVersionNumber;
+
+//! Project version string for ModalStatus.
+FOUNDATION_EXPORT const unsigned char ModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModalStatus/PublicHeader.h>
+
+
